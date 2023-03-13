a4f637ca0e6e0b446e06c247851ab1f535e9b247
Denny Tek <tek.denny@gmail.com>
2023-03-13 14:32:30 -0400

    Edit chart types and properties

--- source/_plugins/mappers/chart_mapper.rb
+++ source/_plugins/mappers/chart_mapper.rb
@@ -5,8 +5,9 @@ class ChartMapper < ::Jekyll::Contentful::Mappers::Base
     chart = super
     markdown_table_html = "#{Kramdown::Document.new(chart["markdown_table"]).to_html}"
     # add chart.css classes
-    # table_classes = "charts-css bar show-primary-axis show-labels show-data-axes"
-    table_classes = "charts-css column show-heading show-labels show-primary-axis show-data-axes  hide-data"
+    # byebug
+    table_classes = "charts-css #{chart["chart_type"]} " + chart["chart_properties"].join(" ")
+    # table_classes = "charts-css  show-heading show-labels show-primary-axis show-data-axes  hide-data"
     markdown_table_html.gsub!("<table>","<table class=\"#{table_classes}\">" )
     chart.merge({ "chart_html" => markdown_table_html })
   end
